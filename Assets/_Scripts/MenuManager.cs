f0d796b4d98dda18c200bbeb69f0fc05fc1ef095
azrazhafir <madindradikaputera1@gmail.com>
2026-06-11 14:00:00 +0700

    aminnnn reaedy

---
+++ Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OpenLevel()
+    {
+        SceneManager.LoadScene("PickLevel1");
+    }
+    public void OpenStage1()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void ToLevel2()
+    {
+        SceneManager.LoadScene("PickLevel2");
+    }
+
+    public void OpenStage2()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+}
